3fc17e509334efc602c10dccb2bd6ee9be6b5c3c
Traian Lupanciuc <traianlupanciuc@gmail.com>
2025-01-26 20:13:39 +0200

    adding function for counting actions for a certain id

--- repository/repository.go
+++ repository/repository.go
@@ -1,14 +1,54 @@
 package repository
 
-import "codingChallenge/model"
+import (
+	"codingChallenge/model"
+	"encoding/json"
+	"fmt"
+)
 
 func SearchID(request model.UserIDGetter) (model.User, error) {
 	var users map[int]model.User
 	var user model.User
-	users, err := dataReader("./repository/data/users.json")
+
+	content, err := dataReader("./repository/data/users.json")
 	if err != nil {
 		return user, err
 	}
 
+	var data []model.User
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		fmt.Printf("Error decoding JSON: %v\n", err)
+		return user, err
+	}
+
+	var userRecords = make(map[int]model.User)
+	for _, val := range data {
+		userRecords[val.ID] = val
+	}
+
 	return users[request.ID], err
+}
+
+func ActionsCount(request model.ActionCountGetter) (model.Count, error) {
+	var count model.Count
+
+	content, err := dataReader("./repository/data/actions.json")
+	if err != nil {
+		return count, err
+	}
+
+	var data []model.Action
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		fmt.Printf("Error decoding JSON: %v\n", err)
+		return count, err
+	}
+
+	for _, val := range data {
+		if val.UserID == request.ID {
+			count.Count++
+		}
+	}
+	return count, nil
 }
