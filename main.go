c8557a0920433470e1619633c5cbcae1636c8667
Traian Lupanciuc <traianlupanciuc@gmail.com>
2025-01-26 19:42:45 +0200

    setting up the server start

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"codingChallenge/controllers"
+	"log"
+	"net/http"
+)
+
 func main() {
-	print("helo worl")
+	mux := controllers.NewRouter()
+	log.Println("Server is starting on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
