f420fce054f81811148bbd57ea6bf62a8044a6af
Traian Lupanciuc <traianlupanciuc@gmail.com>
2025-01-26 20:13:02 +0200

    updating logic for read to include only necessary info

--- repository/reader.go
+++ repository/reader.go
@@ -1,14 +1,12 @@
 package repository
 
 import (
-	"codingChallenge/model"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 )
 
-func dataReader(pathFile string) (map[int]model.User, error) {
+func dataReader(pathFile string) ([]byte, error) {
 	file, err := os.Open(pathFile)
 	if err != nil {
 		return nil, err
@@ -20,17 +18,5 @@ func dataReader(pathFile string) (map[int]model.User, error) {
 		return nil, err
 	}
 
-	var data []model.User // You can also use a struct instead of map
-	err = json.Unmarshal(content, &data)
-	if err != nil {
-		fmt.Printf("Error decoding JSON: %v\n", err)
-		return nil, err
-	}
-
-	var userRecords = make(map[int]model.User)
-	for _, val := range data {
-		userRecords[val.ID] = val
-	}
-
-	return userRecords, nil
+	return content, nil
 }
