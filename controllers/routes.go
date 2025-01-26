ff98bec4e6632bf8f57a94971e81fabb3a931cd0
Traian Lupanciuc <traianlupanciuc@gmail.com>
2025-01-27 00:18:54 +0200

    adding the referralIndex route

--- controllers/routes.go
+++ controllers/routes.go
@@ -7,9 +7,10 @@ import (
 func NewRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/get/", getUserID)
-	mux.HandleFunc("/count/", getUserActionsTotal)
+	mux.HandleFunc("/getUser/", getUserID)
+	mux.HandleFunc("/countActions/", getUserActionsTotal)
 	mux.HandleFunc("/nextAction", getNextActionsGlobal)
+	mux.HandleFunc("/referralIndex", getReferralIndex)
 
 	return mux
 }
